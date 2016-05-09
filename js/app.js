611e5fa0cf9e9501557bcbfdca5ddc318e02baff
IndiraShrestha <indirashrestha.21@gmail.com>
2016-05-09 16:53:04 -0400

    final submission with mulitple changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 /*-------- to define global scope for var randomNum as its a local var in makeAGuess function---------*/
 var randomNum;
+var trackGuesses = 0;//the reason we define trackguesses out here is that we just one se the trackguesses only once. if we have in the function it will reset the value to zero
 
 $(document).ready(function startGame(){
 	
@@ -16,62 +17,96 @@ $(document).ready(function startGame(){
 
   	/*--------- Start a new game -------*/
   	$(".new").click(function(){
-  		startGame();
+  		clearText();
+      makeARandomNum();
+      resetVariable(); 
   	});
 
+
   	/*------ generates random number ----*/	
-  	function makeARandomNum(min,max){
+  	function makeARandomNum(){
   		/*----- by removing var in the randomNum I am setting a global variable in it. Had it had var randomNum in it, it would be local var------*/
-  		randomNum = Math.floor(Math.random()*(max - min + 1)) + min;
+  		randomNum = Math.floor(Math.random()*100)  + 1;
+  		console.log(randomNum);
   	}
 
   	/*------- add the click handler -----------*/
   	$("#guessForm").submit(function(e){
   		e.preventDefault();//this is to prevent from refreshing the page when we click on the button
-  		enterAGuess(); //can i just call the function enterAGuess instead of writing the function (){enterAGuess}
+      var inputNumber = $('#userGuess').val();
+  		if (validateNum(inputNumber) != false){
+        enterAGuess(inputNumber);  
+      }
+      clearText(); 
+      //trackGuesses++;
+      //$("#count").text(trackGuesses);
+      
+
   		//alert("Test");
   	});
-
-  	
   	makeARandomNum(1,100);
+    
+
+  /*----------- validating the user input -------------*/
+  function validateNum(numberToBeValidated){
+    if (numberToBeValidated % 1 != 0){
+      alert("Please input an integer");
+      return false;
+    }
+    else if (numberToBeValidated < 0 || numberToBeValidated > 100){
+      alert("Please input the number between the range of 1 and 100");
+      return false;
+    }
+    clearText();
+    // else if for past guesses ask Casey for help about the past guesses else if 
+  }  
+
+
+  /*----------- defining the function for calculation of hot and cold app--------*/
+  function enterAGuess(guessedNumbered){
+
+    var differenceNum = Math.abs(guessedNumbered - randomNum);
+    if (differenceNum == 0){
+    	//alert("You did it!");
+    	$("h2").text("You Won!");
+    } 
+  	else if (differenceNum >= 50){
+  		$("h2").text("Colder");
+  	}
+    else if (differenceNum >= 30){
+    	$("h2").text("Cold");
+    }
+    else if (differenceNum >=20){
+    	$("h2").text("Hot");
+    }
+    else if (differenceNum >=10){
+    	$("h2").text("Hotter");
+    } 
+    /*---- counting the number of guesses */
+    countTrackGuesses(guessedNumbered);	
+  }
+
+  /*----------- to track the guesses -------*/
+  function countTrackGuesses(numberGuessed) {
+  	trackGuesses++;
+    $("#count").text(trackGuesses);
+    $('#guessList').append( "<li>" + numberGuessed + "</li>")
+  }
+
+  /*----- to clear text box --------*/
+  function clearText(){
+    $('#userGuess').val('');
+  }
+
+
+  /*---- reset variable ---*/
+  function resetVariable(){
+    $("h2").text("Make your Guess");
+    trackGuesses = 0;
+    $('#count').text(trackGuesses);
+    $("#guessList li").remove();//removes the guessed numbered list
+  }
+
+
 });
 
-/*----------- defining the function for calculation of hmmm.....*/
-var inputNumber = $("#userGuess").val();
-
-function enterAGuess (){
-  	if (inputNumber == randomNum){
-  		alert("You did it!");
-  		return $("h2").text("You Won!");
-  	} 
-	  	return checkIceCold();
-  	
-  	 	return checkCold();
-  	
-  	 	return checkHot();
-  	
-}
-
- function checkIceCold(){
- 	var iceColdNum = randomNum + 50
-		if (inputNumber >= iceColdNum || inputNumber <= iceColdNum) {
-			alert("Ice Cold");
-			$("h2").text("Ice Cold");
-		}
-}
-
-function checkCold(){
- 	var coldNum = randomNum + 30
-		if (inputNumber >= coldNum || inputNumber <= coldNum) {
-			alert("Cold");
-			$("h2").text("Cold");
-		}
-}
-
-function checkHot(){
- 	var hotNum = randomNum + 10
-		if (inputNumber >= hotNum || inputNumber <= hotNum) {
-			alert("Hot");
-			$("h2").text("Hot");
-		}
-}
